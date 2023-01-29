47066b2284916fd3dee196bf7276d4b41b673d87
Sebastián <sebastian.zcc@gmail.com>
2023-01-29 13:49:39 -0500

    minor project updates

--- src/main/java/com/sbszc/eduspringbootsecurity/security/ApplicationUserDetails.java
+++ src/main/java/com/sbszc/eduspringbootsecurity/security/ApplicationUserDetails.java
@@ -1,8 +1,6 @@
 package com.sbszc.eduspringbootsecurity.security;
 
 import com.sbszc.eduspringbootsecurity.entity.UserAuth;
-import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
